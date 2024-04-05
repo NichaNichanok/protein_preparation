fc9fc30f400bfb3dcb95e2c1a7ac74bbac065111
Auevechanichkul <nauevech@mob-007146.zib.de>
2024-04-05 11:37:52 +0200

    solved issues in addhcharges.py & add folder

--- add_hcharges.py
+++ add_hcharges.py
@@ -1,15 +1,18 @@
 import os
 
-# this code require the Openbabel install
+# this code requires the OpenBabel installation
 
-def add_hcharges(input_directory, output_directory):
+# Check the results, however some weird molecules show up :/
+
+def add_hcharges(input_directory, output_directory, pH=7.4):
     """
     Process PDB files in the input directory, 
-    protonate them at pH 7.4, and savethem to PDB format.
+    protonate them at pH 7.4, and save them to PDB format.
     
     Args:
     - input_directory (str): Path to the directory containing PDB files.
-    - output_directory (str): Path to the directory for saving modified PDBQT files.
+    - output_directory (str): Path to the directory for saving modified PDB files.
+    - pH (float): pH value for protonation (default is 7.4).
     """
     # Create the output directory if it doesn't exist
     os.makedirs(output_directory, exist_ok=True)
@@ -25,12 +28,9 @@ def add_hcharges(input_directory, output_directory):
                 # Protonated PDB file path
                 protonated_pdb = os.path.join(output_directory, f"{os.path.splitext(filename)[0]}_protonated.pdb")
 
-                # Output PDBQT file path
-                output_pdbqt = os.path.join(output_directory, f"{os.path.splitext(filename)[0]}_protein.pdbqt")
+                # Run the command to protonate at pH 7.4 and conduct local energy minimization on the input PDB file
+                os.system(f"obabel {input_pdb} -opdb -h -p {pH} -O {protonated_pdb}")
 
-                # Run the command to protonate at pH 7.4 and conduct local energy minimizer the input PDB file
-                os.system(f"obabel {input_pdb} -opdb -p 7.4 - O {protonated_pdb}")
-                
                 print(f"Processed {filename}. Protonated file saved to {protonated_pdb}.")
             except Exception as e:
                 print(f"Error processing {filename}: {e}")
