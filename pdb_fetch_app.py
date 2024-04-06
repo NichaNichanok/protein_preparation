4fea46dcc776fcb2076d6f83db6fed322c6b597b
Auevechanichkul <nauevech@mob-007146.zib.de>
2024-04-07 00:03:28 +0200

    changed the view setup

--- pdb_fetch_app.py
+++ pdb_fetch_app.py
@@ -1,5 +1,5 @@
-# This is app is created by Chanin Nantasenamat (Data Professor) https://youtube.com/dataprofessor
-# Credit: This app is inspired by https://huggingface.co/spaces/osanseviero/esmfold
+# This is app is inspired by Chanin Nantasenamat (Data Professor) https://youtube.com/dataprofessor
+
 
 import streamlit as st
 from stmol import showmol
@@ -8,23 +8,41 @@ import requests
 import biotite.structure.io as bsio
 
 #st.set_page_config(layout = 'wide')
-st.sidebar.title('🎈 Protein Preparation from RCSB.com')
-st.sidebar.write('[*ESMFold*](https://esmatlas.com/about) is an end-to-end single sequence protein structure predictor based on the ESM-2 language model. For more information, read the [research article](https://www.biorxiv.org/content/10.1101/2022.07.20.500902v2) and the [news article](https://www.nature.com/articles/d41586-022-03539-1) published in *Nature*.')
+st.sidebar.title('🎈 Visualize protein structure from RCSB.com')
+st.sidebar.write('The code of this page is shown in [*Github*](https://github.com/NichaNichanok) in "pdb_fetch_app.py.')
+
 
 # stmol
 def render_mol(pdb):
     pdbview = py3Dmol.view()
-    pdbview.addModel(pdb,'pdb')
-    pdbview.setStyle({'cartoon':{'color':'spectrum'}})
-    pdbview.setBackgroundColor('white')#('0xeeeeee')
+    pdbview.addModel(pdb, 'pdb')
+    
+    # Set style for protein atoms (displayed as surface with gray color)
+    pdbview.setStyle({'chain': 'A'}, {'cartoon': {'color': 'green'}})
+
+    # Set style for water molecules (displayed as a cross)
+    pdbview.setStyle({'resn': ['HOH', 'WAT']}, {'cross': {}})
+    
+    # Set style for HETATM atoms (displayed in stick style)
+    pdbview.setStyle({'hetflag': True}, {'stick': {'colorscheme':'yellowCarbon'}})
+
+    # Set style for atoms within 5 angstrom of ligand UH7 (displayed in stick style)
+    pdbview.addStyle({'within': {'distance': '5', 'sel': {'organic': True}}}, {'stick': {}})
+    
+    # Set surface for other atoms (except ligands) with VDW representation
+    pdbview.addSurface(py3Dmol.VDW, {'opacity': 0.85, 'color': 'white'}, \
+                       {'not': {'hetflag': True}})
+    
+    pdbview.setBackgroundColor('white')
     pdbview.zoomTo()
-    pdbview.zoom(2, 800)
-    pdbview.spin(True)
-    showmol(pdbview, height = 500,width=800)
+    pdbview.zoom(0.8)  # Adjust zoom level as needed
+    pdbview.spin(True, speed=0.025,)
+    showmol(pdbview, height=500, width=800)
+
 
 # Protein sequence input
-DEFAULT_SEQ = "6o0k"
-txt = st.sidebar.text_area('Input PDB-ID', DEFAULT_SEQ, height=275)
+DEFAULT_SEQ = "1sqt"
+txt = st.sidebar.text_area('Input PDB-ID: with ONLY a lignad', DEFAULT_SEQ, height=275)
 
 
 def get_pdb(pdb_id):
@@ -47,27 +65,14 @@ def update(sequence=txt):
     with open(pdb_filename, 'r') as f:
         pdb_string = f.read()
 
-    struct = bsio.load_structure(pdb_filename, extra_fields=["b_factor"])
-    b_value = round(struct.b_factor.mean(), 4)
 
     # Display protein structure
-    st.subheader('Visualization of predicted protein structure')
+    st.subheader(f'Visualization of the {pdb_id} protein structure')
     render_mol(pdb_string)
 
-    # plDDT value is stored in the B-factor field
-    st.subheader('plDDT')
-    st.write('plDDT is a per-residue estimate of the confidence in prediction on a scale from 0-100.')
-    st.info(f'plDDT: {b_value}')
-
-    st.download_button(
-        label="Download PDB",
-        data=pdb_string,
-        file_name=f'{pdb_id}.pdb',
-        mime='text/plain',
-    )
 
 
-predict = st.sidebar.button('Predict', on_click=update)
+predict = st.sidebar.button("Let's have a look!", on_click=update)
 
 
 if not predict:
